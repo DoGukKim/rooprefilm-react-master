8a5689492c91b47b30def73ca594113f9c5de20c
DOGUK <dgkim3811@naver.com>
2020-08-31 14:03:21 +0900

    AddEslintPrettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxBrackets: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 80,
+};
